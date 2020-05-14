efff2e5a579c72b766f54644e17f96b82533970b
Ron Yanku <ron.yanku86@gmail.com>
2020-05-14 03:10:55 +0300

    mongodb connected

--- src/app/home/list.model.ts
+++
@@ -1,5 +0,0 @@
-export class List {
-  id: string;
-  title: string;
-  creator: string;
-}
--- src/app/home/task.model.ts
+++
@@ -1,7 +0,0 @@
-export class Task {
-  id: string;
-  listId: string;
-  title: string;
-  content: string;
-  price: string;
-}
